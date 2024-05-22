eedc7a72c1a0ccedf97bf439bf31c654cf8341ba
stigs <66088580+stigsec@users.noreply.github.com>
2024-05-22 09:02:53 +0200

    v.1.3
    Changes:
    - added salt encryption
    - removed encryption and decryption of string in the terminal
    - can only encrypt/decrypt from files except salt

---
+++ v1.3/mabc.py
@@ -0,0 +1,30 @@
+import sys, mabcapi
+
+try:
+    if sys.argv[1] == '-s':
+        phrase = sys.argv[2]
+        mabcapi.salt_encrypt(phrase)
+        sys.exit(1)
+    if sys.argv[1] == '-ss':
+        salt = sys.argv[2]
+        phrase = sys.argv[3]
+        mabcapi.split_salt(salt, phrase)
+        sys.exit(1)
+    if sys.argv[1] == '-h':
+        mabcapi.usage()
+        sys.exit(1)
+    if sys.argv[1] == '-v':
+        mabcapi.version()
+        sys.exit(1)
+    else:
+        action = sys.argv[1]
+        pwd1 = sys.argv[2]
+        pwd2 = sys.argv[3]
+        BL = int(sys.argv[4])
+        BD = int(sys.argv[5])
+        phrase = sys.argv[6]
+
+        mabcapi.determine(action, pwd1, pwd2, BL, BD, phrase)
+
+except IndexError:
+    mabcapi.usage()
